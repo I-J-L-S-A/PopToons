c798a07e2a3a71cd81a35d8c2c64f71577700cab
RobertoCuevasP <pablorobertocp1@gmail.com>
2021-10-01 21:09:35 -0400

    Project Structure

---
+++ app/src/main/java/com/ijlsa/poptoons/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ijlsa.poptoons
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

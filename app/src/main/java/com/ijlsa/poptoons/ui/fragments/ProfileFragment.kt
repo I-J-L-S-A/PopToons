da50032986543228ddbdddaa71bd34dbea6b25a4
Cesar Illanes <cesarill99@gmail.com>
2021-10-19 04:54:35 -0400

    Flux added from profile to settings

--- app/src/main/java/com/ijlsa/poptoons/ui/fragments/ProfileFragment.kt
+++ app/src/main/java/com/ijlsa/poptoons/ui/fragments/ProfileFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.navigation.fragment.findNavController
 import com.ijlsa.poptoons.R
 
 class ProfileFragment: StepsBaseFragment() {
@@ -14,5 +16,12 @@ class ProfileFragment: StepsBaseFragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_profile, container, false)
         return  view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val settings = view.findViewById<ImageView>(R.id.ivSettings)
+        settings.setOnClickListener(){
+            findNavController().navigate(R.id.action_profileFragment_to_settingsFragment)
+        }
     }
 }

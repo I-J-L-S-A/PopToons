ec9e335878217196b96d3339c69cdfe70bb64656
luvellana <luciananunezl99@gmail.com>
2021-10-18 23:40:48 -0400

    Fragment desing finished 2

--- app/src/main/java/com/ijlsa/poptoons/ui/fragments/ConfigurationFragment.kt
+++ app/src/main/java/com/ijlsa/poptoons/ui/fragments/ConfigurationFragment.kt
@@ -1,4 +1,19 @@
 package com.ijlsa.poptoons.ui.fragments
 
-class ConfigurationFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ijlsa.poptoons.R
+
+class ConfigurationFragment: StepsBaseFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val view = inflater.inflate(R.layout.fragment_configuration, container, false)
+        //view.background.alpha = 0
+        return view
+    }
 }

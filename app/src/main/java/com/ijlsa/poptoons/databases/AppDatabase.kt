5c13aa70becef132cdacd15ecb061c6c1b36128d
luvellana <luciananunezl99@gmail.com>
2021-10-31 11:40:34 -0400

    DAO, little changes on layout detail fragment and added to AppDatabase

--- app/src/main/java/com/ijlsa/poptoons/databases/AppDatabase.kt
+++ app/src/main/java/com/ijlsa/poptoons/databases/AppDatabase.kt
@@ -2,10 +2,13 @@ package com.ijlsa.poptoons.databases
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.ijlsa.poptoons.databases.daos.EpisodesDao
 import com.ijlsa.poptoons.databases.daos.SeriesDao
+import com.ijlsa.poptoons.ui.model.Episodes
 import com.ijlsa.poptoons.ui.model.Series
 
-@Database(entities = [Series::class], version=1)
+@Database(entities = [Series::class, Episodes::class], version=1)
 abstract class AppDatabase: RoomDatabase(){
     abstract fun seriesDao(): SeriesDao
+    abstract fun episodesDao(): Episodes
 }
--- app/src/main/java/com/ijlsa/poptoons/ui/model/episodes.kt
+++ app/src/main/java/com/ijlsa/poptoons/ui/model/episodes.kt
@@ -7,5 +7,6 @@ import java.io.Serializable
 @Entity
 data class Episodes (@PrimaryKey val id: Int,
                    val serieId: Int,
+                     val numSeason :Int,
                    val eTitle: String,
                    val status: String): Serializable
